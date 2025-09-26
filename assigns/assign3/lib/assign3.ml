b385b1131e9f8b2bb00a743b9aa88ca4113f8983
ChengdaLin <clin73@syr.edu>
2025-09-26 15:34:54 -0400

    delete rev_append

--- assigns/assign3/lib/assign3.ml
+++ assigns/assign3/lib/assign3.ml
@@ -1,9 +1,3 @@
-
-let rec rev_append (l : 'a list) (r : 'a list) : 'a list =
-  match l with
-  | [] -> r
-  | x :: xs -> rev_append xs (x :: r)
-
 let is_whitespace c = List.mem c [' '; '\n'; '\t'; '\r']
 
 let split_on_whitespace (s : string) : string list =
