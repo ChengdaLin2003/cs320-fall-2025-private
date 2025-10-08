91be73fc6bc925e075ac7fa959fc916899d8eed9
ChengdaLin <clin73@syr.edu>
2025-10-08 01:16:23 -0400

    finish assign5

--- assigns/assign5/lib/assign5.ml
+++ assigns/assign5/lib/assign5.ml
@@ -1,15 +1,31 @@
+let curry3 f =
+  fun a ->
+    fun b ->
+      fun c ->
+        f (a, b, c)
 
-let curry3 = assert false
+let uncurry3 f =
+  fun (a, b, c) -> f a b c
 
-let uncurry3 = assert false
-
-let filter_map _ _ = assert false
+let rec filter_map p l =
+  match l with
+  | [] -> []
+  | x :: xs ->
+      match p x with
+      | None -> filter_map p xs
+      | Some y -> y :: filter_map p xs
 
 type 'a rtree = Node of 'a * 'a rtree list
 
-let map_rtree _ _ = assert false
+let rec map_rtree f (Node (v, children)) =
+  Node (f v, List.map (map_rtree f) children)
 
-let filter_rtree _ _ = assert false
+let rec filter_rtree f (Node (v, children)) =
+  if f v then
+    let kept_children = filter_map (filter_rtree f) children in
+    Some (Node (v, kept_children))
+  else
+    None
 
 type op = Add | Sub | Mul | Div
 
@@ -33,8 +49,51 @@ let string_of_op = function
   | Mul -> "*"
   | Div -> "/"
 
-let string_of_expr _ = assert false
+let rec string_of_expr e =
+  match e with
+  | Int n -> string_of_int n
+  | Var x -> x
+  | Call (fname, args) ->
+      let args_str = String.concat ", " (List.map string_of_expr args) in
+      fname ^ "(" ^ args_str ^ ")"
+  | Bop (op, e1, e2) ->
+      "(" ^ string_of_expr e1 ^ " " ^ string_of_op op ^ " " ^ string_of_expr e2 ^ ")"
 
-let string_of_stmt _ = assert false
+  
+let split_on_char c s =
+  let rec aux i j acc =
+    if j = String.length s then
+      List.rev (String.sub s i (j - i) :: acc)
+    else if s.[j] = c then
+      aux (j + 1) (j + 1) (String.sub s i (j - i) :: acc)
+    else
+      aux i (j + 1) acc
+  in
+  aux 0 0 []
 
-let string_of_prog _ =  assert false
+let rec string_of_stmt s =
+  match s with
+  | Assign (x, e) ->
+      x ^ " = " ^ string_of_expr e
+  | Print e ->
+      "print(" ^ string_of_expr e ^ ")"
+  | Return e ->
+      "return " ^ string_of_expr e
+  | FunDef (name, args, body) ->
+      let args_str = String.concat ", " args in
+      let indent_all_lines s =
+        split_on_char '\n' s |> List.map (fun line -> "    " ^ line) |> String.concat "\n"
+      in
+      let body_str =
+        match body with
+        | [] -> "    pass"
+        | _ ->
+            body
+            |> List.map string_of_stmt
+            |> List.map indent_all_lines
+            |> String.concat "\n"
+      in
+      "def " ^ name ^ "(" ^ args_str ^ "):\n" ^ body_str
+
+let string_of_prog (p : prog) =
+  String.concat "\n" (List.map string_of_stmt p)
