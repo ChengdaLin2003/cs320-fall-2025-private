a756eb888cb0b3962fe574788d1f617876e9e77d
ChengdaLin <clin73@syr.edu>
2025-11-02 22:23:16 -0500

    updated

--- assigns/interp1/lib/interp1.ml
+++ assigns/interp1/lib/interp1.ml
@@ -4,7 +4,7 @@ let parse (s : string) : expr option =
   let lb = Lexing.from_string s in
   try Some (Parser.prog Lexer.read lb) with Parser.Error -> None
 
-let rec subst (_v : value) (_x : string) (e : expr) : expr = e
+let subst (_v : value) (_x : string) (e : expr) : expr = e
 
 let eval (_e : expr) : (value, error) result = Error ParseFail
 
