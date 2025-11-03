44b06205a253b4f2ed6c4f0d12d24d54ad91c1d8
ChengdaLin <clin73@syr.edu>
2025-11-02 22:53:18 -0500

    update

--- assigns/interp1/lib/interp1.ml
+++ assigns/interp1/lib/interp1.ml
@@ -18,11 +18,9 @@ let rec subst (v : value) (x : string) (e : expr) : expr =
   | Bop (op, e1, e2) -> Bop (op, subst v x e1, subst v x e2)
   | If (c, t, f) -> If (subst v x c, subst v x t, subst v x f)
   | Let (y, e1, e2) ->
-      if y = x then Let (y, subst v x e1, e2)    (* y 重新绑定，别进 e2 *)
+      if y = x then Let (y, subst v x e1, e2)
       else Let (y, subst v x e1, subst v x e2)
-  | Fun (y, body) ->
-      if y = x then Fun (y, body)                (* y 遮蔽 x *)
-      else Fun (y, subst v x body)
+  | Fun (y, body) -> if y = x then Fun (y, body) else Fun (y, subst v x body)
 
 let expect_num = function VNum n -> Some n | _ -> None
 
@@ -36,89 +34,92 @@ let rec eval (e : expr) : (value, error) result =
   | Fun (x, body) -> Ok (VFun (x, body))
   | App (e1, e2) -> (
       match eval e1 with
-      | Ok (VFun (x, body)) -> (match eval e2 with
-        | Ok v2 -> eval (subst v2 x body)
-        | Error _ as err -> err)
+      | Ok (VFun (x, body)) -> (
+          match eval e2 with
+          | Ok v2 -> eval (subst v2 x body)
+          | Error _ as err -> err)
       | Ok _ -> Error InvalidApp
       | Error _ as err -> err)
-  | Let (x, e1, e2) -> (match eval e1 with
+  | Let (x, e1, e2) -> (
+      match eval e1 with
       | Ok v -> eval (subst v x e2)
       | Error _ as err -> err)
-  | If (c, t, f) -> (match eval c with
+  | If (c, t, f) -> (
+      match eval c with
       | Ok (VBool true) -> eval t
       | Ok (VBool false) -> eval f
       | Ok _ -> Error InvalidIfCond
       | Error _ as err -> err)
   | Bop (op, e1, e2) -> (
       match op with
-      | And -> (* 短路与 *)
-          (match eval e1 with
-           | Ok (VBool false) -> Ok (VBool false)
-           | Ok (VBool true)  ->
-               (match eval e2 with
-                | Ok (VBool b2) -> Ok (VBool b2)
-                | Ok _ -> Error (InvalidArgs And)
-                | Error _ as err -> err)
-           | Ok _ -> Error (InvalidArgs And)
-           | Error _ as err -> err)
-      | Or ->  (* 短路或 *)
-          (match eval e1 with
-           | Ok (VBool true) -> Ok (VBool true)
-           | Ok (VBool false) ->
-               (match eval e2 with
-                | Ok (VBool b2) -> Ok (VBool b2)
-                | Ok _ -> Error (InvalidArgs Or)
-                | Error _ as err -> err)
-           | Ok _ -> Error (InvalidArgs Or)
-           | Error _ as err -> err)
-      | Add | Sub | Mul | Div | Mod | Lt | Lte | Gt | Gte | Eq | Neq ->
-          (match eval e1 with
-           | Error _ as err -> err
-           | Ok v1 ->
-             match eval e2 with
-             | Error _ as err -> err
-             | Ok v2 ->
-               match op with
-               | Add -> (match expect_num v1, expect_num v2 with
-                         | Some n1, Some n2 -> Ok (VNum (n1 + n2))
-                         | _ -> Error (InvalidArgs Add))
-               | Sub -> (match expect_num v1, expect_num v2 with
-                         | Some n1, Some n2 -> Ok (VNum (n1 - n2))
-                         | _ -> Error (InvalidArgs Sub))
-               | Mul -> (match expect_num v1, expect_num v2 with
-                         | Some n1, Some n2 -> Ok (VNum (n1 * n2))
-                         | _ -> Error (InvalidArgs Mul))
-               | Div -> (match expect_num v1, expect_num v2 with
-                         | Some _, Some 0 -> Error DivByZero
-                         | Some n1, Some n2 -> Ok (VNum (n1 / n2))
-                         | _ -> Error (InvalidArgs Div))
-               | Mod -> (match expect_num v1, expect_num v2 with
-                         | Some _, Some 0 -> Error DivByZero
-                         | Some n1, Some n2 -> Ok (VNum (n1 mod n2))
-                         | _ -> Error (InvalidArgs Mod))
-               | Lt  -> (match expect_num v1, expect_num v2 with
-                         | Some n1, Some n2 -> Ok (VBool (n1 < n2))
-                         | _ -> Error (InvalidArgs Lt))
-               | Lte -> (match expect_num v1, expect_num v2 with
-                         | Some n1, Some n2 -> Ok (VBool (n1 <= n2))
-                         | _ -> Error (InvalidArgs Lte))
-               | Gt  -> (match expect_num v1, expect_num v2 with
-                         | Some n1, Some n2 -> Ok (VBool (n1 > n2))
-                         | _ -> Error (InvalidArgs Gt))
-               | Gte -> (match expect_num v1, expect_num v2 with
-                         | Some n1, Some n2 -> Ok (VBool (n1 >= n2))
-                         | _ -> Error (InvalidArgs Gte))
-               | Eq  -> (match v1, v2 with
-                         | VNum n1, VNum n2 -> Ok (VBool (n1 = n2))
-                         | VBool b1, VBool b2 -> Ok (VBool (b1 = b2))
-                         | VUnit, VUnit -> Ok (VBool true)
-                         | _ -> Error (InvalidArgs Eq))
-               | Neq -> (match v1, v2 with
-                         | VNum n1, VNum n2 -> Ok (VBool (n1 <> n2))
-                         | VBool b1, VBool b2 -> Ok (VBool (b1 <> b2))
-                         | VUnit, VUnit -> Ok (VBool false)
-                         | _ -> Error (InvalidArgs Neq))
-               | And | Or -> assert false  (* impossible here; handled above *)))
+      | And -> (
+          match eval e1 with
+          | Ok (VBool false) -> Ok (VBool false)
+          | Ok (VBool true) -> (
+              match eval e2 with
+              | Ok (VBool b2) -> Ok (VBool b2)
+              | Ok _ -> Error (InvalidArgs And)
+              | Error _ as err -> err)
+          | Ok _ -> Error (InvalidArgs And)
+          | Error _ as err -> err)
+      | Or -> (
+          match eval e1 with
+          | Ok (VBool true) -> Ok (VBool true)
+          | Ok (VBool false) -> (
+              match eval e2 with
+              | Ok (VBool b2) -> Ok (VBool b2)
+              | Ok _ -> Error (InvalidArgs Or)
+              | Error _ as err -> err)
+          | Ok _ -> Error (InvalidArgs Or)
+          | Error _ as err -> err)
+      | Add | Sub | Mul | Div | Mod | Lt | Lte | Gt | Gte | Eq | Neq -> (
+          match eval e1 with
+          | Error _ as err -> err
+          | Ok v1 -> (
+              match eval e2 with
+              | Error _ as err -> err
+              | Ok v2 -> (
+                  match op with
+                  | Add -> (match expect_num v1, expect_num v2 with
+                            | Some n1, Some n2 -> Ok (VNum (n1 + n2))
+                            | _ -> Error (InvalidArgs Add))
+                  | Sub -> (match expect_num v1, expect_num v2 with
+                            | Some n1, Some n2 -> Ok (VNum (n1 - n2))
+                            | _ -> Error (InvalidArgs Sub))
+                  | Mul -> (match expect_num v1, expect_num v2 with
+                            | Some n1, Some n2 -> Ok (VNum (n1 * n2))
+                            | _ -> Error (InvalidArgs Mul))
+                  | Div -> (match expect_num v1, expect_num v2 with
+                            | Some _, Some 0 -> Error DivByZero
+                            | Some n1, Some n2 -> Ok (VNum (n1 / n2))
+                            | _ -> Error (InvalidArgs Div))
+                  | Mod -> (match expect_num v1, expect_num v2 with
+                            | Some _, Some 0 -> Error DivByZero
+                            | Some n1, Some n2 -> Ok (VNum (n1 mod n2))
+                            | _ -> Error (InvalidArgs Mod))
+                  | Lt  -> (match expect_num v1, expect_num v2 with
+                            | Some n1, Some n2 -> Ok (VBool (n1 < n2))
+                            | _ -> Error (InvalidArgs Lt))
+                  | Lte -> (match expect_num v1, expect_num v2 with
+                            | Some n1, Some n2 -> Ok (VBool (n1 <= n2))
+                            | _ -> Error (InvalidArgs Lte))
+                  | Gt  -> (match expect_num v1, expect_num v2 with
+                            | Some n1, Some n2 -> Ok (VBool (n1 > n2))
+                            | _ -> Error (InvalidArgs Gt))
+                  | Gte -> (match expect_num v1, expect_num v2 with
+                            | Some n1, Some n2 -> Ok (VBool (n1 >= n2))
+                            | _ -> Error (InvalidArgs Gte))
+                  | Eq  -> (match v1, v2 with
+                            | VNum n1, VNum n2 -> Ok (VBool (n1 = n2))
+                            | VBool b1, VBool b2 -> Ok (VBool (b1 = b2))
+                            | VUnit, VUnit -> Ok (VBool true)
+                            | _ -> Error (InvalidArgs Eq))
+                  | Neq -> (match v1, v2 with
+                            | VNum n1, VNum n2 -> Ok (VBool (n1 <> n2))
+                            | VBool b1, VBool b2 -> Ok (VBool (b1 <> b2))
+                            | VUnit, VUnit -> Ok (VBool false)
+                            | _ -> Error (InvalidArgs Neq))
+                  | And | Or -> assert false))))
 
 let interp (s : string) : (value, error) result =
   match parse s with
