482044f658785612c3d8959559c3be6b8157e6ac
ChengdaLin <clin73@syr.edu>
2025-11-10 21:48:42 -0500

    updaed

--- assigns/interp2/lib/utils.ml
+++ assigns/interp2/lib/utils.ml
@@ -1,34 +1,25 @@
-(* -------------------- Binary Operators -------------------- *)
-(** [bop] represents all binary operators supported in the language.
-    They cover arithmetic, comparison, and boolean connectives. *)
 type bop =
-  | Add | Sub | Mul | Div | Mod              (* arithmetic: + - * / mod *)
-  | Lt | Lte | Gt | Gte | Eq | Neq           (* comparison: < <= > >= = <> *)
-  | And | Or                                 (* boolean logic *)
+  | Add | Sub | Mul | Div | Mod
+  | Lt | Lte | Gt | Gte | Eq | Neq
+  | And | Or
 
-(* -------------------- Abstract Syntax Tree -------------------- *)
-(** [expr] is the abstract syntax tree of expressions. *)
 type expr =
-  | Unit                                     (* () *)
-  | True | False                             (* booleans *)
-  | Num of int                               (* integer literal *)
-  | Var of string                            (* variable reference *)
-  | Let of string * expr * expr              (* let x = e1 in e2 *)
-  | If of expr * expr * expr                 (* if c then t else f *)
-  | Fun of string * expr                     (* fun x -> e *)
-  | App of expr * expr                       (* e1 e2 *)
-  | Bop of bop * expr * expr                 (* e1 op e2 *)
+  | Unit
+  | True | False
+  | Num of int
+  | Var of string
+  | Let of string * expr * expr
+  | If of expr * expr * expr
+  | Fun of string * expr
+  | App of expr * expr
+  | Bop of bop * expr * expr
 
-(* -------------------- Runtime Values -------------------- *)
-(** [value] are runtime values produced by the evaluator. *)
 type value =
   | VUnit
   | VBool of bool
   | VNum of int
-  | VFun of string * expr    (* a simple closure with just parameter & body *)
+  | VFun of string * expr
 
-(* -------------------- Errors -------------------- *)
-(** [error] enumerates all runtime/driver errors we surface. *)
 type error =
   | UnknownVar of string
   | InvalidArgs of bop
@@ -36,8 +27,6 @@ type error =
   | InvalidApp
   | DivByZero
   | ParseFail
-
-(* -------------------- Pretty-printers -------------------- *)
 
 let string_of_bop = function
   | Add -> "+" | Sub -> "-" | Mul -> "*" | Div -> "/" | Mod -> "mod"
@@ -66,7 +55,6 @@ let string_of_value = function
   | VNum n -> string_of_int n
   | VFun _ -> "<fun>"
 
-(* Pretty-print an interpreter error. *)
 let string_of_error = function
   | UnknownVar x     -> "unknown variable: " ^ x
   | InvalidArgs op   -> "invalid operands for operator " ^ string_of_bop op
@@ -75,6 +63,4 @@ let string_of_error = function
   | DivByZero        -> "division by zero"
   | ParseFail        -> "parse failure"
 
-(* -------------------- Top-level program -------------------- *)
-(** The driver expects [prog] to be the same as [expr] at this stage. *)
 type prog = expr
