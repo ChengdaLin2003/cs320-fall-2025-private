63c7699496e1a14519b44c4cae0f329cf0706a2d
Nathan Mull <nmmull@gmail.com>
2025-11-13 09:52:58 -0500

    added demo code

--- lectures/11-13/demo_starter/lib/utils.ml
+++ lectures/11-13/demo_starter/lib/utils.ml
@@ -1,27 +1,35 @@
+type ty =
+  | UnitTy
+  | IntTy
+  | BoolTy
+  | FunTy of ty * ty
+
 type expr =
   | Unit
   | Var of string
   | Num of int
-  | Fun of string * expr
+  | Fun of string * ty * expr
   | Add of expr * expr
   | Eq of expr * expr
   | If of expr * expr * expr
   | App of expr * expr
-  | Let of string * expr * expr
-  | LetRec of string * string * expr * expr
+  | Let of string * ty * expr * expr
+  | LetRec of string * string * ty * ty * expr * expr
 
-type top_let =
-  | Let of string * expr
-  | LetRec of string * string * expr
+type toplet =
+  | TopLet of string * ty * expr
+  | TopLetRec of string * string * ty * ty * expr
 
-type prog = top_let list
+type prog = toplet list
 
 module Env = Map.Make(String)
 
 type value =
+  | VUnit
   | VBool of bool
   | VNum of int
   | VClos of string option * env * string * expr
+
 
 (* Bindings from strings to values *)
 and env = value Env.t
