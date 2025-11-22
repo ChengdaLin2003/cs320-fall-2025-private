2e088a4f93ef260b6a61843b0b5eed116c406dfe
ChengdaLin <clin73@syr.edu>
2025-11-22 12:14:03 -0500

    update

--- assigns/interp2/lib/interp2.ml
+++ assigns/interp2/lib/interp2.ml
@@ -1,6 +1,7 @@
 open Utils
 module Env = Utils.Env
 exception DivByZero = Division_by_zero
+exception AssertFail = Assert_failure
 
 (******************************************************************
  * 重新导出 ty / error / sfexpr / toplet / prog / expr / value 类型，
