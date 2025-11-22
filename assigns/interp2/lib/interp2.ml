b7c83a1facc1ff9d735428e01325a2e3239e0b3d
ChengdaLin <clin73@syr.edu>
2025-11-22 13:17:01 -0500

    update

--- assigns/interp2/lib/interp2.ml
+++ assigns/interp2/lib/interp2.ml
@@ -5,8 +5,7 @@ exception DivByZero = Division_by_zero
 exception AssertFail
 
 (******************************************************************
- * 重新导出 ty / error / sfexpr / toplet / prog / expr / value 类型，
- * 让构造器在 Interp2 中也可见
+ * 重新导出类型: ty / error / sfexpr / toplet / prog / expr / value
  ******************************************************************)
 
 type ty = Utils.ty =
@@ -27,6 +26,11 @@ type error = Utils.error =
   | LetTyErr of ty * ty
   | LetRecErr of string
   | AssertTyErr of ty
+
+type bop = Utils.bop =
+  | Add | Sub | Mul | Div | Mod
+  | Lt | Lte | Gt | Gte | Eq | Neq
+  | And | Or
 
 type sfexpr = Utils.sfexpr =
   | SUnit
@@ -50,14 +54,14 @@ type sfexpr = Utils.sfexpr =
   | SBop of bop * sfexpr * sfexpr
   | SAssert of sfexpr
 
-type toplet = Utils.toplet =
-  {
-    is_rec : bool;
-    name : string;
-    args : (string * ty) list;
-    ty : ty;
-    binding : sfexpr;
-  }
+type toplet = Utils.toplet = {
+  is_rec : bool;
+  name : string;
+  args : (string * ty) list;
+  ty : ty;
+  binding : sfexpr;
+}
+
 type prog = Utils.prog
 
 type expr = Utils.expr =
@@ -78,7 +82,6 @@ type expr = Utils.expr =
     }
   | Assert of expr
 
-(* 重新导出 value / dyn_env，使 grader 可以写 expr -> value *)
 type value = Utils.value =
   | VUnit
   | VBool of bool
@@ -93,138 +96,117 @@ type value = Utils.value =
 and dyn_env = value Env.t
 
 (******************************************************************
- * parse : string -> prog option
+ * Parser
  ******************************************************************)
 
 let parse (s : string) : prog option =
-  (* 成功解析 -> Some prog；任何异常 -> None *)
   try
     let lexbuf = Lexing.from_string s in
-    let p = Parser.prog Lexer.read lexbuf in
-    Some p
+    Some (Parser.prog Lexer.read lexbuf)
   with _ ->
     None
 
-(******************************************************************
- * 把 Utils 里的打印函数转发出来给 main/test 用
- ******************************************************************)
-
-let string_of_value (v : value) : string =
-  Utils.string_of_value v
-
-let string_of_error (e : error) : string =
-  Utils.string_of_error e
+let string_of_value = Utils.string_of_value
+let string_of_error = Utils.string_of_error
 
 (******************************************************************
- * Helper: 构造函数类型 t1 -> t2 -> ... -> tk -> ret_ty
+ * Helper: 构造函数类型
  ******************************************************************)
 
 let rec fun_ty_of_args (args : (string * ty) list) (ret_ty : ty) : ty =
   match args with
   | [] -> ret_ty
-  | (_, t) :: tl ->
-      FunTy (t, fun_ty_of_args tl ret_ty)
+  | (_, t) :: tl -> FunTy (t, fun_ty_of_args tl ret_ty)
 
 (******************************************************************
- * Helper: 柯里化多参数函数体
- *   (x1:t1) ... (xk:tk) 和 已经 desugar 的 body_e
- *   => Fun(x1,t1, Fun(x2,t2, ... body_e))
+ * 柯里化 Fun
  ******************************************************************)
 
 let rec curry_fun (args : (string * ty) list) (body_e : expr) : expr =
   match args with
   | [] -> body_e
-  | (x, t) :: tl ->
-      Fun (x, t, curry_fun tl body_e)
+  | (x, ty) :: tl ->
+      Fun (x, ty, curry_fun tl body_e)
 
 (******************************************************************
- * Helper: 把 f a1 a2 a3 ... 左结合成 ((f a1) a2) a3 ...
+ * 左结合应用链
  ******************************************************************)
 
-let rec chain_app (f : expr) (args : expr list) : expr =
+let rec chain_app f args =
   match args with
   | [] -> f
-  | a :: tl ->
-      chain_app (App (f, a)) tl
+  | a :: tl -> chain_app (App (f, a)) tl
 
 (******************************************************************
- * desugar_sf : sfexpr -> expr
+ * desugar sfexpr -> expr
  ******************************************************************)
 
-let rec desugar_sf (e : sfexpr) : expr =
+let rec desugar_sf e =
   match e with
-  | SUnit        -> Unit
-  | SBool b      -> Bool b
-  | SNum n       -> Num n
-  | SVar x       -> Var x
+  | SUnit -> Unit
+  | SBool b -> Bool b
+  | SNum n -> Num n
+  | SVar x -> Var x
 
   | SFun { args; body } ->
-      let body_e = desugar_sf body in
-      curry_fun args body_e
+      curry_fun args (desugar_sf body)
 
   | SApp lst ->
       (match lst with
-       | [] -> failwith "desugar_sf: empty SApp"
+       | [] -> failwith "empty SApp"
        | f :: args ->
-           let f_e = desugar_sf f in
-           let args_e = List.map desugar_sf args in
-           chain_app f_e args_e)
+           chain_app (desugar_sf f) (List.map desugar_sf args))
 
   | SIf (c, t, f) ->
-      let c_e = desugar_sf c in
-      let t_e = desugar_sf t in
-      let f_e = desugar_sf f in
-      If (c_e, t_e, f_e)
+      If (desugar_sf c, desugar_sf t, desugar_sf f)
 
   | SBop (op, e1, e2) ->
-      let e1' = desugar_sf e1 in
-      let e2' = desugar_sf e2 in
-      Bop (op, e1', e2')
+      Bop (op, desugar_sf e1, desugar_sf e2)
 
   | SAssert e1 ->
-      let e1' = desugar_sf e1 in
-      Assert e1'
+      Assert (desugar_sf e1)
 
   | SLet { is_rec; name; args; ty; binding; body } ->
       let binding_e = curry_fun args (desugar_sf binding) in
       let fun_ty = fun_ty_of_args args ty in
-      let body_e = desugar_sf body in
-      Let { is_rec; name; ty = fun_ty; binding = binding_e; body = body_e }
+      Let {
+        is_rec;
+        name;
+        ty = fun_ty;
+        binding = binding_e;
+        body = desugar_sf body;
+      }
 
-(******************************************************************
- * desugar_toplet : toplet 和 continuation k -> expr
- ******************************************************************)
+let desugar_toplet t k =
+  Let {
+    is_rec = t.is_rec;
+    name = t.name;
+    ty = fun_ty_of_args t.args t.ty;
+    binding = curry_fun t.args (desugar_sf t.binding);
+    body = k;
+  }
 
-let desugar_toplet (t : toplet) (k : expr) : expr =
-  let { is_rec; name; args; ty; binding } = t in
-  let binding_e = curry_fun args (desugar_sf binding) in
-  let fun_ty = fun_ty_of_args args ty in
-  Let { is_rec; name; ty = fun_ty; binding = binding_e; body = k }
-
-(******************************************************************
- * 真正的 desugar : prog -> expr
- ******************************************************************)
-
-let desugar (p : prog) : expr =
+let desugar p =
   match p with
   | [] -> Unit
-  | _  ->
-      let last_name = (List.hd (List.rev p)).name in
-      let ret = Var last_name in
-      List.fold_right desugar_toplet p ret
+  | _ ->
+      let last = (List.hd (List.rev p)).name in
+      List.fold_right desugar_toplet p (Var last)
 
 (******************************************************************
- * 类型检查：expr -> (ty, error) result
+ * Type checking
  ******************************************************************)
 
 type ty_env = ty Env.t
 
-let empty_env : ty_env = Env.empty
+let lookup env x =
+  match Env.find_opt x env with
+  | Some t -> Ok t
+  | None -> Error (UnknownVar x)
 
-let lookup (env : ty_env) (x : string) : (ty, error) result =
-  try Ok (Env.find x env) with Not_found -> Error (UnknownVar x)
+let empty_env = Env.empty
 
-let rec type_of_with (env : ty_env) (e : expr) : (ty, error) result =
+let rec type_of_with env e =
   match e with
   | Unit -> Ok UnitTy
   | Bool _ -> Ok BoolTy
@@ -232,115 +214,194 @@ let rec type_of_with (env : ty_env) (e : expr) : (ty, error) result =
   | Var x -> lookup env x
 
   | If (c, t, f) ->
-      begin
-        match type_of_with env c with
-        | Error e -> Error e
-        | Ok tc ->
-            if tc <> BoolTy then Error (IfCondTyErr tc)
-            else
-              match type_of_with env t, type_of_with env f with
-              | Error e, _ | _, Error e -> Error e
-              | Ok tt, Ok tf ->
-                  if tt = tf then Ok tt else Error (IfTyErr (tt, tf))
+      begin match type_of_with env c with
+      | Error e -> Error e
+      | Ok tc ->
+          if tc <> BoolTy then Error (IfCondTyErr tc)
+          else
+            match type_of_with env t, type_of_with env f with
+            | Ok tt, Ok tf ->
+                if tt = tf then Ok tt else Error (IfTyErr (tt, tf))
+            | Error e, _ | _, Error e -> Error e
       end
 
   | Bop (op, e1, e2) ->
-      begin
-        match type_of_with env e1 with
-        | Error e -> Error e
-        | Ok t1 ->
-            match type_of_with env e2 with
-            | Error e -> Error e
-            | Ok t2 ->
-                begin match op with
-                | Add | Sub | Mul | Div | Mod ->
-                    if t1 <> IntTy then Error (OpTyErrL (op, IntTy, t1))
-                    else if t2 <> IntTy then Error (OpTyErrR (op, IntTy, t2))
-                    else Ok IntTy
-                | Lt | Lte | Gt | Gte ->
-                    if t1 <> IntTy then Error (OpTyErrL (op, IntTy, t1))
-                    else if t2 <> IntTy then Error (OpTyErrR (op, IntTy, t2))
-                    else Ok BoolTy
-                | And | Or ->
-                    if t1 <> BoolTy then Error (OpTyErrL (op, BoolTy, t1))
-                    else if t2 <> BoolTy then Error (OpTyErrR (op, BoolTy, t2))
-                    else Ok BoolTy
-                | Eq | Neq ->
-                    if t1 <> t2 then Error (OpTyErrR (op, t1, t2))
-                    else Ok BoolTy
-                end
+      begin match type_of_with env e1, type_of_with env e2 with
+      | Error e, _ | _, Error e -> Error e
+      | Ok t1, Ok t2 ->
+          begin match op with
+          | Add | Sub | Mul | Div | Mod ->
+              if t1 <> IntTy then Error (OpTyErrL (op, IntTy, t1))
+              else if t2 <> IntTy then Error (OpTyErrR (op, IntTy, t2))
+              else Ok IntTy
+          | Lt | Lte | Gt | Gte ->
+              if t1 <> IntTy then Error (OpTyErrL (op, IntTy, t1))
+              else if t2 <> IntTy then Error (OpTyErrR (op, IntTy, t2))
+              else Ok BoolTy
+          | And | Or ->
+              if t1 <> BoolTy then Error (OpTyErrL (op, BoolTy, t1))
+              else if t2 <> BoolTy then Error (OpTyErrR (op, BoolTy, t2))
+              else Ok BoolTy
+          | Eq | Neq ->
+              if t1 <> t2 then Error (OpTyErrR (op, t1, t2))
+              else Ok BoolTy
+          end
       end
 
   | Fun (x, arg_ty, body) ->
-      let env' = Env.add x arg_ty env in
-      begin
-        match type_of_with env' body with
-        | Ok body_ty -> Ok (FunTy (arg_ty, body_ty))
-        | Error e -> Error e
+      begin match type_of_with (Env.add x arg_ty env) body with
+      | Ok tbody -> Ok (FunTy (arg_ty, tbody))
+      | Error e -> Error e
       end
 
   | App (e1, e2) ->
-      begin
-        match type_of_with env e1 with
-        | Error e -> Error e
-        | Ok tfun ->
-            begin match tfun with
-            | FunTy (arg_ty, res_ty) ->
-                begin
-                  match type_of_with env e2 with
-                  | Error e -> Error e
-                  | Ok targ ->
-                      if targ = arg_ty then Ok res_ty
-                      else Error (FunArgTyErr (arg_ty, targ))
-                end
-            | _ -> Error (FunAppTyErr tfun)
-            end
+      begin match type_of_with env e1 with
+      | Error e -> Error e
+      | Ok (FunTy (arg_ty, ret_ty)) ->
+          begin match type_of_with env e2 with
+          | Ok targ ->
+              if targ = arg_ty then Ok ret_ty
+              else Error (FunArgTyErr (arg_ty, targ))
+          | Error e -> Error e
+          end
+      | Ok t -> Error (FunAppTyErr t)
       end
 
-  | Let { is_rec; name; ty = annot_ty; binding; body } ->
-      if not is_rec then
-        begin
-          match type_of_with env binding with
-          | Error e -> Error e
-          | Ok t_binding ->
-              if t_binding <> annot_ty
-              then Error (LetTyErr (annot_ty, t_binding))
-              else
-                let env' = Env.add name annot_ty env in
-                type_of_with env' body
-        end
-      else
-        begin
-          match binding with
-          | Fun (_, _, _) ->
-              let env_for_binding = Env.add name annot_ty env in
-              begin
-                match type_of_with env_for_binding binding with
-                | Error e -> Error e
-                | Ok t_binding ->
-                    if t_binding <> annot_ty
-                    then Error (LetTyErr (annot_ty, t_binding))
-                    else
-                      let env' = Env.add name annot_ty env in
-                      type_of_with env' body
-              end
-          | _ -> Error (LetRecErr name)
-        end
+  | Let { is_rec; name; ty; binding; body } ->
+      if not is_rec then (
+        match type_of_with env binding with
+        | Ok t_binding ->
+            if t_binding <> ty then Error (LetTyErr (ty, t_binding))
+            else type_of_with (Env.add name ty env) body
+        | Error e -> Error e
+      ) else (
+        (* let rec: binding 必须是一个 function *)
+        match binding with
+        | Fun _ ->
+            let env' = Env.add name ty env in
+            (match type_of_with env' binding with
+             | Ok t_binding ->
+                 if t_binding <> ty then Error (LetTyErr (ty, t_binding))
+                 else type_of_with env' body
+             | Error e -> Error e)
+        | _ -> Error (LetRecErr name)
+      )
 
   | Assert e1 ->
-      begin
-        match type_of_with env e1 with
-        | Error e -> Error e
-        | Ok t ->
-            if t <> BoolTy then Error (AssertTyErr t)
-            else Ok UnitTy
+      begin match type_of_with env e1 with
+      | Ok BoolTy -> Ok UnitTy
+      | Ok t -> Error (AssertTyErr t)
+      | Error e -> Error e
       end
 
-let type_of (e : expr) : (ty, error) result =
-  type_of_with empty_env e
+let type_of e = type_of_with empty_env e
 
-let eval (_e : expr) : value =
-  failwith "eval: TODO"
+(******************************************************************
+ * Runtime evaluation (mp1 semantics)
+ ******************************************************************)
 
-let interp (_s : string) : (value, error) result =
-  Error ParseErr
+let int_of_value = function
+  | VNum n -> n
+  | _ -> failwith "expected int"
+
+let bool_of_value = function
+  | VBool b -> b
+  | _ -> failwith "expected bool"
+
+let rec eval_with env e =
+  match e with
+  | Unit -> VUnit
+  | Bool b -> VBool b
+  | Num n -> VNum n
+  | Var x ->
+      (try Env.find x env with Not_found ->
+         failwith ("unbound variable " ^ x))
+
+  | If (c, t, f) ->
+      if bool_of_value (eval_with env c)
+      then eval_with env t
+      else eval_with env f
+
+  | Fun (x, _, body) ->
+      VClos { arg = x; body; env; name = None }
+
+  | App (e1, e2) ->
+      let vf = eval_with env e1 in
+      let va = eval_with env e2 in
+      begin match vf with
+      | VClos clos ->
+          let env' = Env.add clos.arg va clos.env in
+          eval_with env' clos.body
+      | _ -> failwith "apply non-function"
+      end
+
+  | Let { is_rec = _; name; ty = _; binding; body } ->
+      (* runtime: let 和 let rec 行为相同（已由 typechecker 保证安全） *)
+      let v = eval_with env binding in
+      eval_with (Env.add name v env) body
+
+  | Assert e1 ->
+      if bool_of_value (eval_with env e1) then VUnit
+      else raise AssertFail
+
+  | Bop (op, e1, e2) ->
+      begin match op with
+      | And ->
+          let v1 = eval_with env e1 in
+          (match v1 with
+           | VBool false -> VBool false
+           | VBool true -> VBool (bool_of_value (eval_with env e2))
+           | _ -> failwith "&& expects bool")
+      | Or ->
+          let v1 = eval_with env e1 in
+          (match v1 with
+           | VBool true -> VBool true
+           | VBool false -> VBool (bool_of_value (eval_with env e2))
+           | _ -> failwith "|| expects bool")
+      | Add | Sub | Mul | Div | Mod ->
+          let n1 = int_of_value (eval_with env e1) in
+          let n2 = int_of_value (eval_with env e2) in
+          begin match op with
+          | Add -> VNum (n1 + n2)
+          | Sub -> VNum (n1 - n2)
+          | Mul -> VNum (n1 * n2)
+          | Div ->
+              if n2 = 0 then raise DivByZero else VNum (n1 / n2)
+          | Mod ->
+              if n2 = 0 then raise DivByZero else VNum (n1 mod n2)
+          | _ -> assert false
+          end
+      | Lt | Lte | Gt | Gte ->
+          let n1 = int_of_value (eval_with env e1) in
+          let n2 = int_of_value (eval_with env e2) in
+          let b = match op with
+            | Lt  -> n1 <  n2
+            | Lte -> n1 <= n2
+            | Gt  -> n1 >  n2
+            | Gte -> n1 >= n2
+            | _ -> assert false
+          in
+          VBool b
+      | Eq | Neq ->
+          let v1 = eval_with env e1 in
+          let v2 = eval_with env e2 in
+          let eq =
+            match v1, v2 with
+            | VUnit, VUnit -> true
+            | VBool b1, VBool b2 -> b1 = b2
+            | VNum n1, VNum n2 -> n1 = n2
+            | _ -> failwith "equality on unsupported value"
+          in
+          VBool (if op = Eq then eq else not eq)
+      end
+
+let eval e = eval_with Env.empty e
+
+let interp s =
+  match parse s with
+  | None -> Error ParseErr
+  | Some p ->
+      let e = desugar p in
+      match type_of e with
+      | Error err -> Error err
+      | Ok _ -> Ok (eval e)
